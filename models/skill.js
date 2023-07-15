af093680e1bfce92e568f4edcd6acb278773f31f
Ashley Akins <ashleyakins2015@hotmail.com>
2023-07-14 21:35:16 -0500

    update

--- models/skill.js
+++ models/skill.js
@@ -4,16 +4,37 @@ const skills = [
     {id 8211:, skill: 'Guinuess World Record for Fastest Debugging', done: true}
     ];
 
-    function getAll() {
-        return skills; 
-      }
+    module.exports = {
+      getAll,
+      getOne,
+      create,
+      deleteOne,
+      update
+    };
+    
+    function update(id, updatedSkill) {
+      id = parseInt(id);
+      const skill = skills.find(skill => skill.id === id);
+      Object.assign(skill, updatedSkill);
+    }
+    
+    function deleteOne(id) {
+      id = parseInt(id);
+      const idx = skills.findIndex(skill => skill.id === id);
+      skills.splice(idx, 1);
+    }
+    
+    function create(skill) {
+      skill.id = Date.now() % 1000000;
+      skill.done = false;
+      skills.push(skill);
+    }
+    
     function getOne(id) {
       id = parseInt(id);
-    
       return skills.find(skill => skill.id === id);
     }
-      module.exports = {
-        getAll,
-        getOne
-      };
-      
+    
+    function getAll() {
+      return skills;
+    }
--- server.js
+++ server.js
@@ -3,21 +3,32 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const methodOverride = require('method-override');
 
 const indexRouter = require('./routes/index');
 const skillsRouter = require('./routes/skills');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.use(function(req, res, next) {
+  console.log('Hello SEI!');
+  
+  res.locals.time = new Date().toLocaleTimeString();
+  next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(methodOverride('_method'));
 
 app.use('/', indexRouter);
 app.use('/skills', skillsRouter);
